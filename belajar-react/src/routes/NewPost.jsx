536d6556552d210c817b96fa6ea48c259332bc9b
Anezz12 <harsenaargrtya1@gmail.com>
2024-06-09 19:30:48 +0700

    update

--- belajar-react/src/routes/NewPost.jsx
+++ belajar-react/src/routes/NewPost.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 
 import classes from "./NewPost.module.css";
 import Modal from "../components/Modal";
+import { Link } from "react-router-dom";
 
-function NewPost({ onCancel, onAddPost }) {
+function NewPost({ onAddPost }) {
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
 
@@ -42,9 +43,9 @@ function NewPost({ onCancel, onAddPost }) {
           />
         </p>
         <p className={classes.actions}>
-          <button type="button" onClick={onCancel}>
+          <Link type="button" to="/">
             Cancel
-          </button>
+          </Link>
           <button>Submit</button>
         </p>
       </form>
